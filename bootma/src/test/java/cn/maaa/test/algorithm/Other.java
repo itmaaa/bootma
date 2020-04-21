dde104be36d492306caf9530369d29f2f261c39d
mazh <mazh@jiguang.cn>
2020-04-21 13:56:15 +0800

    fix

--- bootma/src/test/java/cn/maaa/test/algorithm/Other.java
+++ bootma/src/test/java/cn/maaa/test/algorithm/Other.java
@@ -1,5 +1,6 @@
 package cn.maaa.test.algorithm;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -13,7 +14,55 @@ public class Other {
     public static void main(String[] args) {
        // System.out.println(new Other().cal1());
         //System.out.println(new Other().test(16));
-        adjust(new int[]{1,2,6,7,5,4,23,8,9});
+        //adjust(new int[]{1,2,6,7,5,4,23,8,9});
+
+        maxCommonPrefix();
+
+    }
+
+    /**
+     *  最长公共前缀
+     * Leetcode: 编写一个函数来查找字符串数组中的最长公共前缀。如果不存在公共前缀，返回空字符串 ""。
+     *
+     * 示例 1:
+     *
+     * 输入: ["flower","flow","flight"]
+     * 输出: "fl"
+     * Copy to clipboardErrorCopied
+     * 示例 2:
+     *
+     * 输入: ["dog","racecar","car"]
+     * 输出: ""
+     * 解释: 输入不存在公共前缀。
+     *
+     * */
+    public static void maxCommonPrefix(){
+        String[] strs = {"acd","acc","acf","acdd","accdf","accfg"};
+        System.out.println("abd".compareTo("abc"));
+        System.out.println("abd".compareTo("abf"));
+        Arrays.sort(strs);
+        for (String string : strs) {
+            System.out.print(string+"  ");
+        }
+
+        // 数组长度
+        int len = strs.length;
+        // 用于保存结果
+        StringBuilder res = new StringBuilder();
+        // 给字符串数组的元素按照升序排序(包含数字的话，数字会排在前面)
+        Arrays.sort(strs);
+        int m = strs[0].length();
+        int n = strs[len - 1].length();
+        int num = Math.min(m, n);
+        for (int i = 0; i < num; i++) {
+            if (strs[0].charAt(i) == strs[len - 1].charAt(i)) {
+                res.append(strs[0].charAt(i));
+            } else
+                break;
+
+        }
+        System.out.println();
+        System.out.print(res.toString());
     }
 
 
