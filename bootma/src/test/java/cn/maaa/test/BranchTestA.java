3b5a392dba88261ed0010b90d466f75fd5029d1e
mzh <it_mzh@163.com>
2020-06-21 19:14:34 +0800

    cherry pick2

--- bootma/src/test/java/cn/maaa/test/BranchTestA.java
+++ bootma/src/test/java/cn/maaa/test/BranchTestA.java
@@ -12,5 +12,6 @@ public class BranchTestA {
         System.out.println("A分支提交");
         System.out.println("A分支第二次提交");
         System.out.println("cherry pick1");
+        System.out.println("cherry pick2");
     }
 }
