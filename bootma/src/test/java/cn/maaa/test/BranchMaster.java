3d2b650fd9c92967838db8e2ca6f6afd546d32b7
mzh <it_mzh@163.com>
2020-06-21 17:29:13 +0800

    主分支提交

---
+++ bootma/src/test/java/cn/maaa/test/BranchMaster.java
@@ -0,0 +1,13 @@
+package cn.maaa.test;
+
+/**
+ * Description:
+ *
+ * @author mazh
+ * @date 2020/6/21 17:22
+ */
+public class BranchMaster {
+    public static void main(String[] args) {
+        System.out.println("主分支提交");
+    }
+}
