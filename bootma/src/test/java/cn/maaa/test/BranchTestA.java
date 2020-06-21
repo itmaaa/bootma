65ab6a430a41a320aa2d1f39ee63d0a073f54b1a
mzh <it_mzh@163.com>
2020-06-21 17:24:00 +0800

    A分支提交

---
+++ bootma/src/test/java/cn/maaa/test/BranchTestA.java
@@ -0,0 +1,13 @@
+package cn.maaa.test;
+
+/**
+ * Description:
+ *
+ * @author mazh
+ * @date 2020/6/21 17:22
+ */
+public class BranchTestA {
+    public static void main(String[] args) {
+        System.out.println("A分支提交");
+    }
+}
