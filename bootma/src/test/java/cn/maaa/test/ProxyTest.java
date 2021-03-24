35bdeb9f9c14c61aeae69c6457e1b616594839cd
mzh <it_mzh@163.com>
2021-03-24 16:24:38 +0800

    proxy

---
+++ bootma/src/test/java/cn/maaa/test/ProxyTest.java
@@ -0,0 +1,42 @@
+package cn.maaa.test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Description:
+ *
+ * @author mazh
+ * @date 2021/3/24 10:56
+ */
+public class ProxyTest {
+
+
+    public static void main(String[] args) {
+        Cat target = new Cat();
+        Animal proxy = (Animal) Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(),
+                 (proxy1, method, args1) -> {
+                    System.out.println("start");
+                    Object invoke = method.invoke(target, args1);
+                     System.out.println(invoke);
+                    System.out.println("end");
+                    return invoke;
+                });
+        proxy.eat();
+    }
+
+
+}
+
+interface Animal{
+    String eat();
+}
+
+class Cat implements Animal{
+
+    @Override
+    public String eat() {
+        return "fish";
+    }
+}
